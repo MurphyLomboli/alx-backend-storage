d1637c24de8184100e3e759f35cc1a1a54961dd8
MurphyLomboli <murphylomboli@outlook.com>
2023-08-17 20:09:43 -0700

    Project Tasks

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""This module inserts into a document"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    insert a doc in a collection
+    Args:
+        mongo_collection: pymongo collection object
+        kwargs: documents to be inserted
+    """
+
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
